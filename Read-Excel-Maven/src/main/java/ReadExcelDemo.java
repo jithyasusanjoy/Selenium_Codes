fe09b80c0fcbd1b9ed6e06e001be38be7092c15d
jithyasusanjoy <jithya13011994@gmail.com>
2023-02-06 15:55:59 +0530

    Excel read and write

---
+++ Read-Excel-Maven/src/main/java/ReadExcelDemo.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelDemo {
+
+	public static void main(String[] args) throws IOException {
+	FileInputStream f= new FileInputStream(new File("D:\\Student.xlsx"));
+    try (XSSFWorkbook wb = new XSSFWorkbook(f)) {
+		XSSFSheet ws= wb.getSheetAt(0);
+		for(Row row: ws)     //iteration over row using for each loop  
+		{  
+		for(Cell cell: row)    //iteration over cell using for each loop  
+		{  
+			System.out.print(cell+" ");
+		}
+		System.out.println();
+		}
+	}
+   
+}
+}
