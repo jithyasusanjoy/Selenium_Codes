63eb72aacaad9f6ab287d4359792ce490f53436c
jithyasusanjoy <jithya13011994@gmail.com>
2023-02-07 05:45:27 +0530

    download code

--- TestNGSelinium/src/test/java/samplepack/DownloadFile.java
+++ TestNGSelinium/src/test/java/samplepack/DownloadFile.java
@@ -1,5 +1,31 @@
 package samplepack;
 
-public class DownloadFile {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class DownloadFile {
+	
+	public WebDriver driver;
+	@Test
+	public void browserload() throws InterruptedException
+	{
+	    driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://filesamples.com/formats/csv");
+		Thread.sleep(3000);
+		Assert.assertEquals(true,driver.findElement(By.xpath("//h1[text()='Sample CSV Files Download']")).isDisplayed());
+		System.out.println("site loaded");	
+		driver.findElement(By.cssSelector("a[href='/samples/document/csv/sample4.csv'] "));
+		Thread.sleep(1000);
+//		Assert.assertEquals(true,driver.findElement(By.xpath("//embed[@type='application/pdf']")).);
+//		System.out.println("File Downloaded");
+	
+	}
+	
+	
+	
+	
 }
