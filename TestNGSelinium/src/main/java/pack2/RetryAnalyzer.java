fb74a800753bef3fd1cf8710cfd0ba93b47955d0
jithyasusanjoy <jithya13011994@gmail.com>
2023-03-06 12:20:50 +0530

    latest changes

---
+++ TestNGSelinium/src/main/java/pack2/RetryAnalyzer.java
@@ -0,0 +1,27 @@
+package pack2;
+
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer  { 
+
+	  private int count = 0; 
+
+	  private int maxCount = 2;
+
+	  public boolean retry(ITestResult result) { 
+
+	    if(count < maxCount) {  
+
+	       count++;
+
+	       return true;        
+
+	    }        
+
+	    return false; 
+
+	 }
+
+	}
---
+++ TestNGSelinium/src/test/java/Testcases/TestTry.java
@@ -0,0 +1,46 @@
+package Testcases;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import pack2.RetryAnalyzer;
+
+@Listeners(pack2.ListenerTest.class)
+public class TestTry {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+
+  @Test(retryAnalyzer=RetryAnalyzer.class)
+
+  public void testFail() {
+
+	  driver.get("https://www.daraz.lk/");
+		
+		driver.findElement(By.id("q")).click();
+		driver.findElement(By.id("q")).clear();
+		driver.findElement(By.id("q")).sendKeys("mobile");
+		driver.findElement(By.xpath("//div[@id='topActionHeader']/div/div[2]/div/div[2]/form/div/div[2]/button")).click();
+
+		Assert.assertTrue(false);
+
+   }
+  @AfterTest
+  public void close() {
+		driver.quit();
+  }
+
+}
